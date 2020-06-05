9e0c3e595ca1e65d818af7434ee9d1cce0f68ba2
anita <anitasafitri6@gmail.com>
2020-06-05 18:53:40 +0700

    file
    file

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2020 at 01:45 PM
+-- Generation Time: Jun 05, 2020 at 01:52 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ecommerce_codeigniter(1)`
+-- Database: `ecommerce`
 --
 
 -- --------------------------------------------------------
