93f6c6903ca340ba8f0536ff3a473097c3411529
ahfarli <ahfarli@users.noreply.github.com>
2020-06-13 18:58:41 +0700

    Update order_success.php

--- application/views/front/order_success.php
+++ application/views/front/order_success.php
@@ -6,7 +6,7 @@
 			<h3>Dear <?php echo $this->session->userdata("cus_name");?></h3>
 <h4>Your Order Successfully complete....</h4><hr/>
 			
-<h6>Total payable amount(including vat) : Rp <?php echo $this->session->userdata("g_total");?></h6>
+<h6>Total payable amount(including vat) : Rp <?php echo $this->session->userdata("g_total");?>00</h6>
 
 <h5 style="text-align: justify;">Thanks for purchase. Recfeive your order successfully. We will contact you ASAP with delivery details. For more details please check your registration mail.</h5>
 			
